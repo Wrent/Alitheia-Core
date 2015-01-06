130db04ac21c83934ad2e229e0c5b4c1355c88eb
Adam Kučera <adam.kucera@wrent.cz>
2015-01-06 17:14:49 +0100

    Created first basic test.

---
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/WebadminServiceImplTest.java
@@ -0,0 +1,25 @@
+package eu.sqooss.test.service.webadmin;
+
+import eu.sqooss.impl.service.webadmin.WebadminServiceImpl;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Adam Kucera
+ */
+public class WebadminServiceImplTest {
+    
+    static WebadminServiceImpl impl;
+    
+    @BeforeClass
+    public static void setUp() {
+        impl = new WebadminServiceImpl();
+    }
+
+    @Test
+    public void testAdminServiceImpl() {
+        assertNotNull(impl);
+    }
+    
+}
