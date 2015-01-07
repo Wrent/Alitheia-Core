15c566973405d0dca90d7369dfdf65390a565af7
mjduijn <maartenduijn@msn.com>
2015-01-07 14:14:56 +0100

    Added first webadminrenderer tests

---
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/WebAdminRendererTest.java
@@ -0,0 +1,64 @@
+package eu.sqooss.test.service.webadmin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import eu.sqooss.impl.service.webadmin.WebAdminRenderer;
+import eu.sqooss.service.scheduler.Scheduler;
+import eu.sqooss.service.scheduler.SchedulerStats;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito.*;
+import org.mockito.runners.*;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WebAdminRendererTest {
+
+	//static WebAdminRenderer ;
+	@Mock Scheduler sobjSched;
+	@InjectMocks WebAdminRenderer renderer;
+	
+
+    @BeforeClass
+    public static void setUp() {
+    	//BundleContext bc = new BundleContext();
+    	//bc.setProperty("eu.sqooss.logbuffer.pattern")
+    	
+    	//renderer = new WebAdminRenderer(null, null);
+    	
+    	//Set up the AlitheiaCore instance
+    	//AlitheiaCore.testInstance(); bc can't be null
+//    	
+//    	AlitheiaCore core = new AlitheiaCore(bc);
+    }
+    
+    @Test
+    public void testWebadminServiceImpl() {
+        assertNotNull(renderer);
+    }
+    
+    @Test
+    public void testRenderJobFailStats() {
+    	//given(taxService.getCurrentTaxFactorFor(any(Person.class))).willReturn(TAX_FACTOR);
+    	MockitoAnnotations.initMocks(this);
+    	
+    	
+    	
+    	when(sobjSched.getSchedulerStats()).thenReturn(new SchedulerStats());
+    	String str = WebAdminRenderer.renderJobFailStats();
+    }
+    
+    
+}
